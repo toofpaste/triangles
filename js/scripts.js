930c769355cef4a7f51e2b6491d67f574a5bf58e
Marc Davies and Michael Larragueta <michael@>
2019-03-28 12:02:34 -0700

    Add everything

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(function(){
+  var i = 0;
+  while (active){
+  var gender = parseInt($("#gender").val());
+  var life = parseInt($("#life").val());
+  console.log(gender);
+  console.log(life);
+
+  if(gender === 1 && life === 3){
+    $("maleA").removeClass();
+    $("#maleA").toggle();
+    alert("3");
+  }else if (gender === 1 && life === 4){
+    alert("2");
+    $("maleL").removeClass();
+    $("#maleL").toggle();
+  }else if (gender === 2 && life === 3){
+    $("#femA").removeClass();
+    $("#femA").toggle();
+  }else if(gender === 2 && life === 4){
+    $("#femL").removeClass();
+    $("#femL").toggle();
+  }else alert("Fuck me in the ass because i love jesus");
+
+};
+
+
+
+});
