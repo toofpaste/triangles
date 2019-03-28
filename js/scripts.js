385677f639c68d573e6f7aa81abbe9640dff93d1
Marc Davies and Michael Larragueta <marc+michael@>
2019-03-28 13:42:03 -0700

    update commit from Atom
    add styles format

--- js/scripts.js
+++ js/scripts.js
@@ -3,33 +3,24 @@ $(function(){
   $("form#select").submit(function(event){
       var gender = parseInt($("#gender").val());
       var life = parseInt($("#life").val());
-      console.log(gender);
-      console.log(life);
       if(gender === 1 && life === 3){
         $("#maleA").removeClass();
-        $("#maleL").addClass("hide");
-        $("#femL").addClass("hide");
-        $("#femA").addClass("hide");
+        $("#maleL, #femL, #femA").addClass("hide");
         $("#maleA").toggle();
       }else if (gender === 1 && life === 4){
         $("#maleL").removeClass();
-        $("#maleA").addClass("hide");
-        $("#femL").addClass("hide");
-        $("#femA").addClass("hide")
+        $("#maleA, #femL, #femA").addClass("hide");
         $("#maleL").toggle();
       }else if (gender === 2 && life === 3){
         $("#femA").removeClass();
-        $("#maleL").addClass("hide");
-        $("#femL").addClass("hide");
-        $("#maleA").addClass("hide")
+        $("#maleL, #femL, #maleA").addClass("hide");
         $("#femA").toggle();
       }else if(gender === 2 && life === 4){
         $("#femL").removeClass();
-        $("#maleL").addClass("hide");
-        $("#maleA").addClass("hide");
-        $("#femA").addClass("hide")
+        $("#maleL, #maleA, #femA").addClass("hide");
         $("#femL").toggle();
       }else alert("Nothing");
+
 
       event.preventDefault();
     });
