1e8355e1aa71b30db25119a124f0135d9b05466e
Marc Davies and Michael Larragueta <michael@>
2019-03-28 13:31:27 -0700

    Add working js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,39 @@
 $(function(){
-  var i = 0;
-  while (active){
-  var gender = parseInt($("#gender").val());
-  var life = parseInt($("#life").val());
-  console.log(gender);
-  console.log(life);
 
-  if(gender === 1 && life === 3){
-    $("maleA").removeClass();
-    $("#maleA").toggle();
-    alert("3");
-  }else if (gender === 1 && life === 4){
-    alert("2");
-    $("maleL").removeClass();
-    $("#maleL").toggle();
-  }else if (gender === 2 && life === 3){
-    $("#femA").removeClass();
-    $("#femA").toggle();
-  }else if(gender === 2 && life === 4){
-    $("#femL").removeClass();
-    $("#femL").toggle();
-  }else alert("Nothing");
+  $("form#select").submit(function(event){
+      var gender = parseInt($("#gender").val());
+      var life = parseInt($("#life").val());
+      console.log(gender);
+      console.log(life);
+      if(gender === 1 && life === 3){
+        $("#maleA").removeClass();
+        $("#maleL").addClass("hide");
+        $("#femL").addClass("hide");
+        $("#femA").addClass("hide");
+        $("#maleA").toggle();
+      }else if (gender === 1 && life === 4){
+        $("#maleL").removeClass();
+        $("#maleA").addClass("hide");
+        $("#femL").addClass("hide");
+        $("#femA").addClass("hide")
+        $("#maleL").toggle();
+      }else if (gender === 2 && life === 3){
+        $("#femA").removeClass();
+        $("#maleL").addClass("hide");
+        $("#femL").addClass("hide");
+        $("#maleA").addClass("hide")
+        $("#femA").toggle();
+      }else if(gender === 2 && life === 4){
+        $("#femL").removeClass();
+        $("#maleL").addClass("hide");
+        $("#maleA").addClass("hide");
+        $("#femA").addClass("hide")
+        $("#femL").toggle();
+      }else alert("Nothing");
 
-};
+      event.preventDefault();
+    });
+
 
 
 
